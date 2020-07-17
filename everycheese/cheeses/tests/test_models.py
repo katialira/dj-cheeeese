23d52b8cfc2133a35970574617a685b8d5fb7651
Katia Lira <katialira.villena@gmail.com>
2020-07-17 15:13:07 -0500

    Tests fixed

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,16 +1,11 @@
 import pytest
 
-from everycheese.cheeses.models import Cheese
+from ..models import Cheese
 from .factories import CheeseFactory
 
 pytestmark = pytest.mark.django_db
 
 def test__str__():
-    cheese = CheeseFactory(name="Stracchino")
-    # cheese = Cheese.objects.create(
-    #     name = "Stracchino",
-    #     description = "Semi-sweet cheese that goes well with starches.",
-    #     firms = Cheese.Firmness.SOFT,
-    # )
-    assert cheese.__str__() == "Stracchino"
-    assert str(cheese) == "Stracchino"
+    cheese = CheeseFactory()
+    assert cheese.__str__() == cheese.name
+    assert str(cheese) == cheese.name
